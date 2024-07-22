0ea6c792e7f8c3fda22c98f2d78d188cb63cdb4f
michaelitindi <michaelitindi@gmail.com>
2024-07-22 16:21:44 +0300

    started working on header nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
 
   return (
     <>
-      <h1 className="h-screen text-center text-3xl font-bold flex justify-center items-center underline">
-        React + Vite + TailwindCSS
-      </h1>
+     <Header/>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+
+function Header() {
+    return (
+        <div className="sticky top 0 flex justify-between top-10 pt-5 pl-3 pb-3 border-b 2 w-full">
+            <div className="logo">Paradise Nursery</div>
+            <div className="">
+                <ul >
+                    <li className="float-left pr-5">one</li>
+                    <li className="float-left pr-5" >two</li>
+                    <li className="float-left pr-5">three</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header
