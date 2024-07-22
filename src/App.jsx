c0471b525b0fa1a3b9f3bf876cc8d3a1ac7d45d9
michaelitindi <michaelitindi@gmail.com>
2024-07-22 20:52:34 +0300

    Add background image and some text.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
-import Header from './components/Header'
+import Landing from './pages/Landing'
 
 function App() {
 
   return (
     <>
-     <Header/>
+     <Landing/>
+     
     </>
   )
 }
