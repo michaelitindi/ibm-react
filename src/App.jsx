29daf60c4d634273991bc608151b06c7af9cd5d6
michaelitindi <michaelitindi@gmail.com>
2024-07-24 11:12:01 +0300

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
+import Landing from './pages/Landing'
 import Productpage from './pages/Productpage'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
 
 function App() {
 
   return (
-    <>
-     <Productpage/>
-     
-    </>
+    <Router>
+      <Routes>
+     <Route exact path="/" element={<Landing/>} />
+     <Route exact path="/products" element={<Productpage/>} />
+     </Routes>
+    </Router>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         <div className="sticky top 0 flex justify-between pl-3 pb-10 border-b 2 w-full mb-10 bg-green-700 ">
+            <Link to={"/"}>
             <div className="text-3xl text-white text-center pt-4">Paradise Nursery</div>
+            </Link>
             <div className="">
             <FontAwesomeIcon icon={faShoppingCart} className="text-white text-2xl pt-5 pr-5" />
             </div>
