fa306e4b81c33b35b273187c9222a189abadb9aa
michaelitindi <michaelitindi@gmail.com>
2024-07-24 16:46:41 +0300

    Deploy

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Landing from './pages/Landing'
+import Productpage from './pages/Productpage'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+
+
+function App() {
+
+  return (
+    <Router>
+      <Routes>
+     <Route exact path="/" element={<Landing/>} />
+     <Route exact path="/products" element={<Productpage/>} />
+     </Routes>
+    </Router>
+  )
+}
+
+export default App
